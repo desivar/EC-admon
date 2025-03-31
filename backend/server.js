2b5108e6d5dbb011cda0fc7cd0653538b0b7bd76
desivar <jillian_desire@yahoo.es>
2025-03-31 14:33:07 -0600

    server content

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`); // This line is important
+});
