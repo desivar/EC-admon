e171cae368a72c0a70b65259e1601e66082f80dd
desivar <jillian_desire@yahoo.es>
2025-03-31 19:19:49 -0600

    upadte

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const connectDB = require('./config/database');
-const userRoutes = require('./routes/user.routes');
-console.log('userRoutes object:', userRoutes); // ADD THIS LINE
-const englishConnect1Routes = require('./routes/englishConnect1Routes');
-const englishConnect2Routes = require('./routes/englishConnect2Routes');
-const englishConnect3Routes = require('./routes/englishConnect3Routes');
+const testRoutes = require('./routes/testRoutes');
 
 require('dotenv').config();
 connectDB();
 app.use(express.json());
 
-console.log('Type of userRoutes:', typeof userRoutes);
-app.use('/api/auth', userRoutes);
-app.use('/api/englishconnect1', englishConnect1Routes);
-app.use('/api/englishconnect2', englishConnect2Routes);
-app.use('/api/englishconnect3', englishConnect3Routes);
+console.log('Type of testRoutes:', typeof testRoutes);
+console.log('testRoutes object:', testRoutes);
+app.use('/api/test', testRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the English Connect Backend!');
