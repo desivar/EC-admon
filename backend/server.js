077cc2a5bbf3060c992c6d69ec164848a778cf2e
desivar <jillian_desire@yahoo.es>
2025-04-01 06:46:34 -0600

    test simplifying version

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,9 @@
-console.log('Starting server...');
 const express = require('express');
 const app = express();
 const port = 3000;
-const connectDB = require('./config/database');
-const stakeRoutes = require('./routes/stakeRoutes');
-
-
-require('dotenv').config();
-connectDB();
-app.use(express.json());
-
-app.use('/api/stakes', stakeRoutes);
-
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the English Connect Backend!');
+    res.send('Hello from Express!');
 });
 
 app.listen(port, () => {
