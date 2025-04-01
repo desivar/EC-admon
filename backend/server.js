536af444499a38e417cc567372ba60a8f65e5e2a
desivar <jillian_desire@yahoo.es>
2025-03-31 19:02:17 -0600

    update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+console.log('Starting server...'); // Add this at the very top
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -11,16 +12,16 @@ require('dotenv').config();
 connectDB();
 app.use(express.json());
 
-console.log('Type of userRoutes:', typeof userRoutes); // Add this line
+console.log('Type of userRoutes:', typeof userRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/api/englishconnect1', englishConnect1Routes);
 app.use('/api/englishconnect2', englishConnect2Routes);
 app.use('/api/englishconnect3', englishConnect3Routes);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the English Connect Backend!');
+    res.send('Welcome to the English Connect Backend!');
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
