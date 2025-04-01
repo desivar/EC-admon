2111b3086a615e919958ddbd24a9017c7e63e17b
desivar <jillian_desire@yahoo.es>
2025-04-01 17:15:57 -0600

    userRoutes.j, User.js,  userController.js

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ const app = express();
 const port = 3000;
 const connectDB = require('./config/database');
 const stakeRoutes = require('./routes/stakeRoutes'); // Import stake routes
+const userRoutes = require('./routes/userRoutes');
 
 require('dotenv').config();
 connectDB();
 app.use(express.json());
 
 app.use('/api/stakes', stakeRoutes); // Mount stake routes at /api/stakes
+app.use('/api/users',userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the English Connect Backend!');
