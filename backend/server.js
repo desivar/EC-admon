1bbbbfd29faa6a117b73c402b3f2270ce7951881
desivar <jillian_desire@yahoo.es>
2025-04-01 21:47:49 -0600

    server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 console.log('Starting server...');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; // Use process.env.PORT for deployment
+const port = 3000;
 const connectDB = require('./config/database');
 const stakeRoutes = require('./routes/stakeRoutes'); // Import stake routes
 const userRoutes = require('./routes/userRoutes');
@@ -11,10 +11,9 @@ require('dotenv').config();
 connectDB();
 app.use(express.json());
 
-// Mount routes
-app.use('/api/users', userRoutes);
 app.use('/api/stakes', stakeRoutes); // Mount stake routes at /api/stakes
-app.use('/api/wards', wardRoutes);   // Mount ward routes at /api/wards
+app.use('/api/users', userRoutes);
+app.use('/api/wards', wardRoutes); // Mount ward routes at /api/wards
 
 app.get('/', (req, res) => {
     res.send('Welcome to the English Connect Backend!');
