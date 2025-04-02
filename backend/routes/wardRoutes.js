77b07dec357ef1f960ccfc67f782838b0d1a20fe
desivar <jillian_desire@yahoo.es>
2025-04-02 05:51:43 -0600

    test if this '' is working instead of'/wards'

--- backend/routes/wardRoutes.js
+++ backend/routes/wardRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const WardController = require('../controllers/WardController');
 
-router.post('/wards', WardController.createWard); // Use the controller function here
+router.post('', WardController.createWard); // Use the controller function here
 
 module.exports = router;
