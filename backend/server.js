99fd5ac04650b323aa4930eaa9e7c7a4b8feef1c
desivar <jillian_desire@yahoo.es>
2025-04-02 21:49:03 -0600

    update

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,9 @@ const userRoutes = require('./routes/userRoutes');
 const wardRoutes = require('./routes/wardRoutes');
 const teacherRoutes = require('./routes/teacherRoutes');
 const studentRoutes = require('./routes/studentRoutes');
-const englishConnect1Routes = require('./routes/englishConnect1Routes');
-const englishConnect2Routes = require('./routes/englishConnect2Routes');
-const englishConnect3Routes = require('./routes/englishConnect3Routes');
+const englishConnect1Routes = require('./routes/englishConnect1Routes'); // Import EC1 routes
+const englishConnect2Routes = require('./routes/englishConnect2Routes'); // Import EC2 routes
+const englishConnect3Routes = require('./routes/englishConnect3Routes'); // Import EC3 routes
 
 require('dotenv').config();
 connectDB();
@@ -20,11 +20,13 @@ app.use('/api/stakes', stakeRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/wards', wardRoutes);
 app.use('/api/teachers', teacherRoutes);
-app.use('/api/students', studentRoutes);
 
-app.use('/api/students/ec1', englishConnect1Routes);
-app.use('/api/students/ec2', englishConnect2Routes);
-app.use('/api/students/ec3', englishConnect3Routes);
+app.use('/api/students', studentRoutes); // Base route for students
+
+// Mount EnglishConnect routes under /api/students
+app.use('/api/students', englishConnect1Routes);
+app.use('/api/students', englishConnect2Routes);
+app.use('/api/students', englishConnect3Routes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the English Connect Backend!');
